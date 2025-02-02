017e9df1cf73188b7427062385dfdb2ac1c6d2d1
skoterev <skoterev@ford.com>
2025-02-02 13:33:37 -0500

    Refactoring Bottles: extract add_verse() method

--- bottles.h
+++ bottles.h
@@ -6,8 +6,14 @@ public:
     static std::vector<std::string> song()
     {
         std::vector<std::string> song;
+        add_verse(song);
+        return song;
+    }
+
+private:
+    static void add_verse(std::vector<std::string>& song)
+    {
         song.push_back("99 bottles of beer on the wall, 99 bottles of beer.");
         song.push_back("Take one down and pass it around, 98 bottles of beer on the wall.");
-        return song;
     }
 };
