a01e926969528da02b1ace06d5b2cee7db6a85d4
skoterev <skoterev@ford.com>
2025-02-02 13:54:22 -0500

    Refactoring tests: Create BottlesTest fixture class, extract test_verse() method

--- test_bottles.cpp
+++ test_bottles.cpp
@@ -2,12 +2,25 @@
 
 #include <gmock/gmock.h>
 
-using ::testing::IsEmpty;
+class BottlesTest : public testing::Test {
+public:
+    static void SetUpTestSuite() { song = Bottles::song(); }
 
-TEST(Bottles, Song)
+    static void TearDownTestSuite() { song.clear(); }
+
+    void test_verse()
+    {
+        ASSERT_GE(song.size(), 2);
+        ASSERT_EQ(song[0], "99 bottles of beer on the wall, 99 bottles of beer.");
+        ASSERT_EQ(song[1], "Take one down and pass it around, 98 bottles of beer on the wall.");
+    }
+
+    static std::vector<std::string> song;
+};
+
+std::vector<std::string> BottlesTest::song;
+
+TEST_F(BottlesTest, Song)
 {
-    std::vector<std::string> song = Bottles::song();
-    ASSERT_GE(song.size(), 2);
-    ASSERT_EQ(song[0], "99 bottles of beer on the wall, 99 bottles of beer.");
-    ASSERT_EQ(song[1], "Take one down and pass it around, 98 bottles of beer on the wall.");
+    test_verse();
 }
