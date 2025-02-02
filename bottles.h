20afb3972dbc6c956848317f68cf737e6656c5ca
skoterev <skoterev@ford.com>
2025-02-02 13:23:10 -0500

    Test the first verse (Null to Constant)

--- bottles.h
+++ bottles.h
@@ -3,5 +3,11 @@
 
 class Bottles {
 public:
-    static std::vector<std::string> song() { return {}; }
+    static std::vector<std::string> song()
+    {
+        std::vector<std::string> song;
+        song.push_back("99 bottles of beer on the wall, 99 bottles of beer.");
+        song.push_back("Take one down and pass it around, 98 bottles of beer on the wall.");
+        return song;
+    }
 };
--- test_bottles.cpp
+++ test_bottles.cpp
@@ -6,5 +6,8 @@ using ::testing::IsEmpty;
 
 TEST(Bottles, Song)
 {
-    EXPECT_THAT(Bottles::song(), IsEmpty());
+    std::vector<std::string> song = Bottles::song();
+    ASSERT_GE(song.size(), 2);
+    ASSERT_EQ(song[0], "99 bottles of beer on the wall, 99 bottles of beer.");
+    ASSERT_EQ(song[1], "Take one down and pass it around, 98 bottles of beer on the wall.");
 }
