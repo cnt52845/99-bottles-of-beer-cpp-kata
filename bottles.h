7b4f93660be9bb53007f577409852537fffdf472
skoterev <skoterev@ford.com>
2025-02-03 00:15:51 -0500

    Test the penultimate verse (Split flow)

--- bottles.h
+++ bottles.h
@@ -6,7 +6,7 @@ public:
     static std::vector<std::string> song()
     {
         std::vector<std::string> song;
-        for (int bottle_count = max_bottles; bottle_count >= 2; bottle_count--) {
+        for (int bottle_count = max_bottles; bottle_count >= 1; bottle_count--) {
             add_verse(song, bottle_count);
         }
         return song;
@@ -15,10 +15,17 @@ public:
 private:
     static void add_verse(std::vector<std::string>& song, int bottle_count)
     {
-        song.push_back(std::to_string(bottle_count) + " bottles of beer on the wall, " +
-                       std::to_string(bottle_count) + " bottles of beer.");
-        song.push_back("Take one down and pass it around, " + std::to_string(bottle_count - 1) +
-                       " bottles of beer on the wall.");
+        if (bottle_count <= 1) {
+            song.push_back("1 bottle of beer on the wall, 1 bottle of beer.");
+            song.push_back(
+                "Take one down and pass it around, no more bottles of beer on the wall.");
+        }
+        else {
+            song.push_back(std::to_string(bottle_count) + " bottles of beer on the wall, " +
+                           std::to_string(bottle_count) + " bottles of beer.");
+            song.push_back("Take one down and pass it around, " + std::to_string(bottle_count - 1) +
+                           " bottles of beer on the wall.");
+        }
     }
 
     static const int max_bottles = 99;
--- test_bottles.cpp
+++ test_bottles.cpp
@@ -26,6 +26,22 @@ public:
         ASSERT_EQ(song[second_line_index], expected_2nd_line);
     }
 
+    void test_penultimate_verse()
+    {
+        const int         verse             = max_verse - 1;
+        const int         first_line_index  = verse * 2;
+        const int         second_line_index = verse * 2 + 1;
+        const int         min_song_size     = verse * 2 + 2;
+        const std::string expected_1st_line = "1 bottle of beer on the wall, 1 bottle of beer.";
+        const std::string expected_2nd_line =
+            "Take one down and pass it around, no more bottles of "
+            "beer on the wall.";
+
+        ASSERT_GE(song.size(), min_song_size);
+        ASSERT_EQ(song[first_line_index], expected_1st_line);
+        ASSERT_EQ(song[second_line_index], expected_2nd_line);
+    }
+
     static const int                max_verse = 99;
     static std::vector<std::string> song;
 };
@@ -37,4 +53,5 @@ TEST_F(BottlesTest, Song)
     for (int verse = 0; verse <= max_verse - 2; verse++) {
         test_verse(verse);
     }
+    test_penultimate_verse();
 }
